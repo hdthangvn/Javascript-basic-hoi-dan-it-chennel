d1d5596b2ec4a18ed0d535befa2b2105213b0457
Hoang Duc Thang <ducthanghoang892004@gmail.com>
2024-06-24 08:52:47 +0700

    source chapter 2, video 6

--- video4/video4.js
+++
@@ -1,6 +0,0 @@
-let a = ` Tôi là String - chuỗi  `;
-console.log(a);
-console.log(4 + 5 + `a`);
-console.log(`a` + 5 + 4);
-console.log(a.length);
-console.log(a.trim().length);
---
+++ video6/video6.js
@@ -0,0 +1,12 @@
+console.log(`Hello word from HTML`.toLocaleUpperCase());
+let a = 5; // int,long,float, ko phân biệt
+let b = 6;
+let c = 6.9;
+console.log(a + b);
+console.log(a + c);
+let d = `5`;
+let e = Number(d);  // let e = +d;
+let f = `haha`;
+console.log(`e value:`, e    , `type:`, typeof e);
+console.log(a + d);
+console.log(a/f);
